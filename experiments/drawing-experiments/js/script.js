229e4b522ac3d366288a27b02ed0a16b9acc5dc1
stphnieD <stphnie.dang@gmail.com>
2020-09-09 22:59:33 -0400

    Drawing colors

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -14,25 +14,42 @@ function setup() {
   // (r,g,b)
   background(255,150,150);
 
-  // rectangle
-  stroke('purple');
-  fill('orange');
+  // rectangles
   rectMode(CENTER); //anchor point
+
+  stroke(120,120,200);
+  fill('orange');
   rect(250,250,150,100,20,50,20,50); //(x,y,w,h,tl,tr,br,bl)
+
+  stroke('orange');
+  fill(120,120,200);
   rect(250,250,20,200,20,50,20,50);
 
-  // circle
-  fill('pink');
+  // circles
+  stroke('red');
   circle(250,250,20)  // (x,y,d)
 
-  // point
+  // points
   stroke('blue');
   point(250,250);   // (x,y)
 
-  // line
+  // lines
   stroke('white');
   line(0,0,500,500); //(start-point-x,start-point-y,end-point-x,end-point-y)
   line(500,0,0,500);
+
+  // Ellipses
+  ellipseMode(CORNER);
+  noStroke();
+
+  fill(102, 204, 255,200);
+  ellipse(250,250,100);
+
+  fill(202, 204, 265,200);
+  ellipse(250,250,80,80);
+
+  fill(302, 204, 275,200);
+  ellipse(250,250,50,50);
 }
 
 // draw()
