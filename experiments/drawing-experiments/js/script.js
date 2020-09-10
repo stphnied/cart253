e13267c9a5905b284d00208941c1080a835d4b39
stphnieD <stphnie.dang@gmail.com>
2020-09-09 22:42:20 -0400

    Drawing experiments

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -9,7 +9,30 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
 
+  // (r,g,b)
+  background(255,150,150);
+
+  // rectangle
+  stroke('purple');
+  fill('orange');
+  rectMode(CENTER); //anchor point
+  rect(250,250,150,100,20,50,20,50); //(x,y,w,h,tl,tr,br,bl)
+  rect(250,250,20,200,20,50,20,50);
+
+  // circle
+  fill('pink');
+  circle(250,250,20)  // (x,y,d)
+
+  // point
+  stroke('blue');
+  point(250,250);   // (x,y)
+
+  // line
+  stroke('white');
+  line(0,0,500,500); //(start-point-x,start-point-y,end-point-x,end-point-y)
+  line(500,0,0,500);
 }
 
 // draw()
