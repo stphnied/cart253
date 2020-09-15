8edfe7522e1e517d11da7138b58722be43e397f5
stphnieD <stphnie.dang@gmail.com>
2020-09-15 15:06:59 -0400

    Changing background color with mouseX

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -16,10 +16,11 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background('lightpink');
-  //background(mouseX, mouseY, mouseX, mouseY/2); // change bg color with mouse
+  background(mouseX,0,0);
   rectMode(CENTER);
   rect(width/2, height/2, 100, 100) // /2 = center
+  
+  //background(mouseX, mouseY, mouseX, mouseY/2); // change bg color with mouse
   // rect(mouseX, mouseY, 100, 100); //current location of the mouse
   //rect(250,250,mouseX,mouseY); //resize the rect with mouse
 
