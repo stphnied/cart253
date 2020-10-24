87db7590d75e0e51204ab8f78dfde7a823e4ea57
stphnieD <stphnie.dang@gmail.com>
2020-10-24 19:52:56 -0400

    8.1 : Object oriented programming

---
+++ experiments/oop-experiments/js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// import flower from `Flower.js`;
+
+/**************************************************
+Object-oriented programming
+Stephanie Dang
+
+**************************************************/
+// Our garden
+let garden = {
+  // An array to store the individual flowers
+  flowers: [],
+  // How many flowers in the garden
+  numFlowers: 20,
+  // The color of the grass (background)
+  grassColor: {
+    r: 120,
+    g: 180,
+    b: 120
+  }
+};
+
+// setup() creates the canvas and the flowers in the garden
+function setup() {
+  createCanvas(600, 600);
+
+  // Create our flowers by counting up to the number of the flowers
+  for (let i = 0; i < garden.numFlowers; i++) {
+    // Create a new flower
+    let flower = new Flower();
+    // Add the flower to the array of flowers
+    garden.flowers.push(flower);
+  }
+}
+
+// draw()
+// Displays our flowers
+function draw() {
+  // Display the grass
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+
+  // Loop through all the flowers in the array and display them
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+    flower.display();
+  }
+}
