6ac2693108af1bd936938e64c369db41207dfc1c
stphnieD <stphnie.dang@gmail.com>
2020-10-25 00:29:32 -0400

    E5: Started exercise 5

---
+++ exercises/exercise5/README.md
@@ -0,0 +1,3 @@
+# Exercise 5 : Juggle garden!
+
+https://stphnied.github.io/cart253/exercises/exercise5/
---
+++ exercises/exercise5/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Exercise 05 : Juggling garden
+Stephanie Dang
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
