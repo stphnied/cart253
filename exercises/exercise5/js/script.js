a8879a83954f7b38eef803a16957a3ff86d448e6
stphnieD <stphnie.dang@gmail.com>
2020-10-25 15:27:28 -0400

    E5 : Reusing activity 07
    added bouncing off walls (x)

--- exercises/exercise5/js/script.js
+++ exercises/exercise5/js/script.js
@@ -1,22 +1,46 @@
 "use strict";
 
 /**************************************************
-Exercise 05 : Juggling garden
+Activity : Juggling simulation
 Stephanie Dang
 
-Here is a description of this template p5 project.
+A juggling simulation with O.O.P.
 **************************************************/
+let gravityForce = 0.0025;
+let paddle;
+let balls = [];
+let numBalls = 10;
+
 
 // setup()
-//
-// Description of setup() goes here.
 function setup() {
+    createCanvas(windowWidth,windowHeight);
 
+    paddle = new Paddle(400,50);
+
+    for(let i =0 ; i <numBalls; i++) {
+        let x = random(0,width);
+        let y = random(-100,-500);
+        let ball = new Ball(x,y);
+        balls.push(ball);
+    }
 }
 
 // draw()
-//
-// Description of draw() goes here.
 function draw() {
+    background(0);
 
+    paddle.move();
+    paddle.display();
+
+    for(let i = 0 ; i< balls.length; i++) {
+        let ball = balls[i];
+        if(ball.active) {
+            ball.gravity(gravityForce);
+            ball.move();
+            ball.bounce(paddle);
+            ball.display();
+        }
+        
+    }
 }
