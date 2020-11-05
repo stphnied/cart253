55bf8463f101733e81d1829480ed4c376ced14ca
stphnieD <stphnie.dang@gmail.com>
2020-11-05 00:20:12 -0500

    sounds experiment 02

--- experiments/sounds-experiments/js/script.js
+++ experiments/sounds-experiments/js/script.js
@@ -1,35 +1,61 @@
 "use strict";
-/**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
-**************************************************/
 
 let barkSFX;
-
-// setup()
-//
-// Description of setup() goes here.
+let oscillator;
+let angle =0;
+let t = 0;
 
 function preload() {
     barkSFX = loadSound('assets/sounds/bark.wav');
 }
+
 function setup() {
-    createCanvas(500,500);
+    createCanvas(600, 600);
     userStartAudio();
+
+    oscillator = new p5.Oscillator(440, `sine`);
+    oscillator.amp(0.1);
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
     background(200);
 
-    let newRate = map(mouseX, 0, width, -3, 3);
-    barkSFX.rate(newRate);
+    // let newRate = map(mouseX, 0, width, -3, 3);
+    // barkSFX.rate(newRate);
+    userCtl2();
 }
 
 function mousePressed() {
-    barkSFX.loop();
+    // barkSFX.loop();
+    oscillator.start();
+}
+
+function mouseReleased() {
+    oscillator.stop();
+}
+
+function userCtl() {
+    let newFreq = map(mouseY, height, 0,20,20000);
+    oscillator.freq(newFreq);
+
+    let newAmp = map(mouseX, 0, width, 0, 1);
+    oscillator.amp(newAmp);
+
+    push();
+    textSize(32);
+    textAlign(LEFT, CENTER);
+    fill(255);
+    text(newFreq, 50, height / 2);
+    pop();
+}
+
+function userCtl2() {
+    let noiseValue = noise(t);
+    let r = random(0,1);
+    let sinAngle = sin(angle);
+    let newFreq = map(noiseValue,-1,1,0,880);
+    oscillator.freq(newFreq);
+
+    angle+=0.1;
+    t+=0.1;
 }
