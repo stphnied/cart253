ca4f650f741f59d10c103f03cf38428299edf090
stphnieD <stphnie.dang@gmail.com>
2020-11-05 16:59:44 -0500

    A9 : Started the acvitity!

---
+++ activities/09-musical-toy/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**************************************************
+Activity 09 : Musical toy
+Stephanie Dang
+
+
+**************************************************/
+
+// setup()
+function setup() {
+
+}
+
+// draw()
+function draw() {
+
+}
